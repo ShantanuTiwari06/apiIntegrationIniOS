7b87106b1baea22660f53dd0d1f229fe4f048ff5
ShantanuTiwari06 <shantanut710@gmail.com>
2020-12-04 16:32:52 +0530

    Initial Commit

---
+++ API_Integration/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  API_Integration
+//
+//  Created by Shantanu Tiwari on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let url = "https://api.sunrise-sunset.org/json?date=2020-12-04&lat=36.7201600&lng=-4.4203400&formatted=0"
+        getData(from: url)
+    }
+    
+    private func getData(from url: String) {
+    let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+            guard let data = data, error == nil else {
+                print("Something Went Wrong")
+                return
+            }
+            
+            // have data
+            var result: Response?
+            do {
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }
+            catch {
+                print("failed to convert \(error.localizedDescription)")
+            }
+            
+            guard let json = result else {
+                return
+            }
+            
+            print(json.status)
+            print(json.results.sunrise)
+            print(json.results.sunset)
+            print(json.results.solar_noon)
+        })
+       task.resume()
+    }
+}
+
+struct Response: Codable {
+    let results: MyResult
+    let status: String
+}
+
+struct MyResult: Codable {
+    let sunrise: String
+    let sunset: String
+    let solar_noon: String
+    let day_length: Int
+    let civil_twilight_begin: String
+    let civil_twilight_end: String
+    let nautical_twilight_begin: String
+    let nautical_twilight_end: String
+    let astronomical_twilight_begin: String
+    let astronomical_twilight_end: String
+}
+
